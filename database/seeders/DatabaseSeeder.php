c5e3306984bc3bc5476bb956e440fa786844fabe
Zidannn10 <zidanazzahra916@gmail.com>
2025-02-26 14:34:13 +0700

    bct

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Subject;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserRolePermissionSeeder::class,
-            PermissionSeeder::class
+            PermissionSeeder::class,
+            SubjectSeeder::class,
         ]);
     }
 }
--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -4,14 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Subject;
 
 class SubjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
         //
+        $data = [
+            ["id" => "1", "name" => "Ambient Outdoor Air Quality"],
+            ["id" => "2", "name" => "Workplace Air Quality"],
+            ["id" => "3", "name" => "Noise"],
+            ["id" => "4", "name" => "Waste Water"]
+        ];
+
+        foreach ($data as $x) {
+            if(!Subject::where('id', $x['id'])->first()){
+                $m = new Subject();
+                $m->id = $x['id'];
+                $m->name = $x['name'];
+                $m->save();
+            }
+        }
     }
 }
